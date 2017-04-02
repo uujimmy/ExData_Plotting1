1f8eab161404d05ffec759a36b28f115a1b84125
uujimmy <uujimmy@gmail.com>
2017-04-02 19:01:10 +0800

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -2,7 +2,7 @@
 df <- read.table("household_power_consumption.txt", sep=";", header=TRUE)
 sub_df <- df[df$Date %in% c("1/2/2007","2/2/2007"),]
 sub_df$datetime = strptime(paste(sub_df$Date, sub_df$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
-png("plot1.png", width=480, height=480)
+png("plot4.png", width=480, height=480)
 
 
 

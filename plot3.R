d8d3493adffd8174c42bac2fde77b0a46d13ec13
uujimmy <uujimmy@gmail.com>
2017-04-02 18:52:28 +0800

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -2,7 +2,7 @@ df <- read.table("household_power_consumption.txt", sep=";", header=TRUE, string
 sub_df <- df[df$Date %in% c("1/2/2007","2/2/2007"),]
 sub_df$datetime = strptime(paste(sub_df$Date, sub_df$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 png("plot3.png", width=480, height=480)
-plot(sub_df$datetime, as.numeric(sub_df$Sub_metering_1), type="l", xlab="", ylab="Global Active Power (kilowatts)")
+plot(sub_df$datetime, as.numeric(sub_df$Sub_metering_1), type="l", xlab="", ylab="Energy Submetering")
 lines(sub_df$datetime, as.numeric(sub_df$Sub_metering_2), type="l", col="red")
 lines(sub_df$datetime, as.numeric(sub_df$Sub_metering_3), type="l", col="blue")
 legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), col=c("black","red","blue"), lty=1, lwd=2)
